dc628500b1cf454de3f8b8485e2356408e9514dc
M. Hoffmann <git@applications-cologne.com>
2020-12-26 12:49:39 +0100

    tests/bootstrap.php completed

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,5 @@
 <?php
 require_once 'vendor/autoload.php';
-require_once 'src/Category.php';
-require_once 'src/Channel.php';
 require_once 'src/enums/Protocol.php';
 require_once 'src/traits/Category.php';
 require_once 'src/traits/Description.php';
@@ -10,6 +8,8 @@ require_once 'src/traits/Link.php';
 require_once 'src/traits/PubDate.php';
 require_once 'src/traits/Title.php';
 require_once 'src/traits/UrlAttribute.php';
+require_once 'src/Category.php';
+require_once 'src/Channel.php';
 require_once 'src/Cloud.php';
 require_once 'src/Enclosure.php';
 require_once 'src/Guid.php';
